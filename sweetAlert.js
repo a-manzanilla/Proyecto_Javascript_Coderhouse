57935310180b901011dcbf91c21e7d65db3e0217
amanzanilla <manzanilla.t@gmail.com>
2022-05-27 02:42:16 -0300

    Se hizo responsive

---
+++ sweetAlert.js
@@ -0,0 +1,15 @@
+//SweetAlert
+Swal.fire({
+    title: '¡Bienvenido a tu librería online!',
+    text: '"Para viajar no hay mejor nave que un libro"',
+    imageUrl: 'https://www.tematika.com/media/resized/media/wysiwyg/Banners_Home/02romper_818_378.jpg',
+    width: 1000,
+    padding: 10,
+    confirmButtonColor:'#FF6962',
+    showClass: {
+        popup: 'animate__animated animate__fadeInDown'
+      },
+      hideClass: {
+        popup: 'animate__animated animate__fadeOutUp'
+      }
+});
